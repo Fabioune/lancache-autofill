a2e86b339149f35e7ec3d8f8c3e8b99366de07d6
fabioune <fabien.bauculat@gmail.com>
2019-04-09 16:48:04 +0200

    dsfsdf

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-GREEN='\033[0;32m'
-BLACK='\033[0m'
+
 
 # printf "${GREEN}Installing dependencies with apt${BLACK}\n"
 # apt install -y  lib32gcc1 \
@@ -9,14 +7,3 @@ BLACK='\033[0m'
 #                 lib32tinfo5 \
 #                 libcurl3-gnutls:i386 \
 
-printf "${GREEN}Installing dependencies with Composer${BLACK}\n"
-cd $SCRIPT_DIR && composer update
-
-printf "${GREEN}Installing Steam${BLACK}\n"
-mkdir -p /usr/games/steam && cd /usr/games/steam && curl -sqL "http://media.steampowered.com/client/steamcmd_linux.tar.gz" | tar zxvf -
-
-cd $SCRIPT_DIR && ./lancache-autofill app:initialise-database
-
-cd $SCRIPT_DIR && ./lancache-autofill steam:update-app-list
-
-cd $SCRIPT_DIR && ./lancache-autofill steam:authorise-account $DEFAULT_STEAM_USER
